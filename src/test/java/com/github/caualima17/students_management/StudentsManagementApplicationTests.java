45b655ee5fd73ef9b72e842d74de7371ce7e67e8
Cauã de Lima <caua-l1@outlook.com>
2025-01-26 08:02:41 -0300

    chore: iniciando o projeto spring com a configuração base

---
+++ src/test/java/com/github/caualima17/students_management/StudentsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.caualima17.students_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
